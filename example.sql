d5c5c9b4cb85d6a21bf234809f93b54939461b10
tanimutomo <tanimutomo@gmail.com>
2020-08-23 14:07:38 +0900

    add: drop/create table in example.sql

--- example.sql
+++ example.sql
@@ -1,3 +1,23 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS articles;
+
+CREATE TABLE users (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  created_at DATETIME,
+  updated_at DATETIME
+);
+
+CREATE TABLE articles (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  user_id BIGINT,
+  title VARCHAR(255),
+  content TEXT,
+  created_at DATETIME,
+  updated_at DATETIME
+);
+
 INSERT INTO users ( -- users table
   id, name, email, created_at, updated_at
 ) VALUES (
@@ -5,9 +25,9 @@ INSERT INTO users ( -- users table
 );
 
 INSERT INTO articles ( -- articles table
-  id, title, content, created_at, updated_at
+  id, user_id, title, content, created_at, updated_at
 ) VALUES (
-  1, 'title1', 'content1', now(), now() -- post1
+  1, 1, 'title1', 'content1', now(), now() -- post1
 ), (
-  2, 'title2', 'content2', now(), now() -- post2
+  2, 1, 'title2', 'content2', now(), now() -- post2
 );
